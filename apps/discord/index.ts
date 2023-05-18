a67b31550ad4c679a75b341e9844151cf2b8a598
Jack LaFond <jack.lafond@icloud.com>
2023-05-18 17:19:19 -0400

    feat(bot): command handler

--- apps/discord/index.ts
+++ apps/discord/index.ts
@@ -1,5 +1,14 @@
-import { Client, GatewayIntentBits, REST, Routes } from "discord.js";
+import {
+  ActivityType,
+  Client,
+  GatewayIntentBits,
+  REST,
+  Routes,
+} from "discord.js";
 import { chatCommandsMap } from "./commands";
+import { log } from "./lib/logger/log";
+import { handleInteraction } from "./interactions";
+require("dotenv").config();
 
 const client = new Client({
   intents: [
@@ -13,21 +22,35 @@ const restClient = new REST({ version: "10" }).setToken(
   process.env.DISCORD_TOKEN as string
 );
 
-client.once("ready", async () => {
+client.on("ready", async () => {
+  await client.user?.setPresence({
+    activities: [
+      {
+        name: "with your mom",
+        type: ActivityType.Playing,
+      },
+    ],
+    status: "online",
+  });
+
   try {
-    await restClient.put(
+    const commandsRegistered = (await restClient.put(
       Routes.applicationGuildCommands(
-        process.env.APPLICATION_ID as string,
-        process.env.GUILD_ID as string
+        process.env.DISCORD_APPLICATION_ID as string,
+        process.env.DISCORD_GUILD_ID as string
       ),
-      { body: chatCommandsMap }
+      { body: [...chatCommandsMap.values()] }
+    )) as Array<unknown>;
+    log.info(
+      `Successfully registered ${commandsRegistered.length} application commands.`
     );
-    console.log("Successfully registered application commands.");
   } catch (error) {
-    console.error(error);
+    log.fatal(error);
   }
 
-  console.log(`Logged in as ${client.user!.tag}!`);
+  log.success(`Logged in as ${client.user!.tag}!`);
 });
+
+client.on("interactionCreate", handleInteraction);
 
 void client.login(process.env.DISCORD_TOKEN as string);
