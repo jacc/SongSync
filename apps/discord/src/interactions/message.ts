cb5fdb8285d8862f32459bbabf106dd812ea20a3
Jack LaFond <jack.lafond@icloud.com>
2023-05-20 00:23:34 -0400

    feat(db): drop kysely for prisma

--- apps/discord/src/interactions/message.ts
+++ apps/discord/src/interactions/message.ts
@@ -1,6 +1,41 @@
 import { Message } from "discord.js";
+import { z } from "zod";
+import getSongs from "../lib/songs";
+import database from "../lib/database/client";
+
+const linkSchema = z.string().refine((x) => {
+  return (
+    x.includes("open.spotify.com/track") ||
+    x.includes("open.spotify.com/album") ||
+    x.includes("music.apple.com") ||
+    x.includes("soundcloud.com")
+  );
+}, "");
 
 export async function handleChatMessage(message: Message): Promise<void> {
   console.log(message.content);
   if (!message.guild || message.author.bot) return;
+
+  const url = linkSchema.safeParse(message.content);
+  if (!url.success) return;
+
+  const matches = url.data.match(/\bhttps?:\/\/\S+/gi);
+  if (!matches) return;
+
+  const serverObject = await database
+    .insertInto("Guild")
+    .values({
+      id: message.guild.id,
+    })
+    .ignore()
+    .returning("Guild.enabled")
+    .execute();
+
+  console.log(serverObject);
+
+  matches.map(async (match) => {
+    const matchLinks = await getSongs(match);
+
+    await message.reply(JSON.stringify(matchLinks, null, 2));
+  });
 }
--- apps/discord/src/lib/database/client.ts
+++ apps/discord/src/lib/database/client.ts
@@ -1,15 +1,3 @@
-import { Kysely } from "kysely";
-import { PlanetScaleDialect } from "kysely-planetscale";
-import { DB } from "../../../prisma/generated/types";
-import { env } from "../../env";
+import { PrismaClient } from "@prisma/client";
 
-const database = new Kysely<DB>({
-  dialect: new PlanetScaleDialect({
-    host: env.DATABASE_HOST,
-    username: env.DATABASE_USERNAME,
-    password: env.DATABASE_PASSWORD,
-    useSharedConnection: true,
-  }),
-});
-
-export default database;
+export const prisma = new PrismaClient();
